a759df87103bf7ad1e6cc60507c4428615658d3b
rsbertoa90 <rsbertoa90@gmail.com>
2019-03-06 11:22:31 -0300

    defer some things

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <link rel="image_src" href="{{asset('/storage/images/app/logo.png')}}">
+    <link  rel="image_src" href="{{asset('/storage/images/app/logo.png')}}">
     <link rel="canonical" href="{{URL::current()}}">
     @if (isset($meta))
         <title> {{$meta->metatitle}} </title>
@@ -20,13 +20,12 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link defer rel="dns-prefetch" href="https://fonts.gstatic.com">
 
     {{-- fontawesome --}}
     {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous"> --}}
     <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link defer href="{{ mix('css/app.css') }}" rel="stylesheet">
     {{-- <link href="{{ asset('css/animate.css') }}" rel="stylesheet"> --}}
 
     {{-- PRECONECTS --}}
